621f6c1abdd4c3531417e07fce027dc5fdca3903
Matthew Brighton Smith <brightonsmith@gmail.com>
2024-09-26 13:43:12 -0400

    lab complete

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plants, plantImgs, prices }) {
+  const [inStock, setInStock] = useState(true);
+
+  const toggleStock = () => {
+    setInStock(!inStock);
+  };
+
+  const plant = plants;
+  const plantImg = plantImgs;
+  const price = prices;
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plantImg} alt={plant} />
+      <h4>{plant}</h4>
+      <p>Price: {price}</p>
+      <button className={inStock ? "primary" : ""} onClick={toggleStock}>
+        {inStock ? "In Stock" : "Out of Stock"}
+      </button>
     </li>
   );
 }
